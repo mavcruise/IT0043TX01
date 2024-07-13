343a404571c8277e80fde5f06e6df56c07ccf871
mavcruise <167377169+mavcruise@users.noreply.github.com>
2024-07-14 00:37:27 +0800

    Create Finals.js

---
+++ Finals.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".reebok-icon");
+    menu.classList.toggle("open")
+    reebokIcon.classList.toggle("open")
+}
